d5e64219852ebcb7b8c558ac297c650280ca00f3
Jedibezy <Jdocton@gmail.com>
2021-09-27 17:05:43 -0700

    Twitter edits

--- pages/therapy.js
+++ pages/therapy.js
@@ -89,7 +89,7 @@ if you are seeking help but don't want to go from therapist to therapist trying 
                     likes="28"
                     retweets="8"
                     comments="3"
-                    media="https://pbs.twimg.com/profile_images/1419119433704898560/87PN7qUD_400x400.jpg"
+                    media="https://pbs.twimg.com/profile_images/1441512323848359944/r7vgRy5x_400x400.jpg"
                   />
                 </div>
               }
