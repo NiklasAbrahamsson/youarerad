94302e97a91a182ca24ee5e7ef5072a27df28543
theo <me@t3.gg>
2021-10-18 23:19:38 -0700

    More simplification

--- src/backend/router/checkout/d/get-checkout.ts
+++ src/backend/router/checkout/d/get-checkout.ts
@@ -6,12 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 })
 
 export default async function getSessionById(id: string) {
-  if (!id.startsWith('cs_')) {
-    throw Error('Incorrect CheckoutSession ID.')
-  }
-  const checkout_session: Stripe.Checkout.Session = await stripe.checkout.sessions.retrieve(id, {
+  return await stripe.checkout.sessions.retrieve(id, {
     expand: ['payment_intent'],
   })
-
-  return checkout_session
 }
--- src/backend/router/checkout/index.ts
+++ src/backend/router/checkout/index.ts
@@ -1,11 +1,12 @@
 import * as trpc from '@trpc/server'
-import * as trpcNext from '@trpc/server/adapters/next'
 import { z } from 'zod'
 import getSessionById from './d/get-checkout'
 
 export const checkoutRouter = trpc.router().query('get-session', {
   input: z.object({
-    id: z.string(),
+    id: z
+      .string()
+      .refine((id) => id.startsWith('cs_'), { message: 'Incorrect CheckoutSession ID.' }),
   }),
   async resolve({ input }) {
     return await getSessionById(input.id)
