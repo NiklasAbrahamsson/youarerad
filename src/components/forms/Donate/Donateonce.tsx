c33c2c42cdb0f88cfbb300aaed53e0a2ad09a983
Jedibezy <Jdocton@gmail.com>
2021-10-19 00:18:00 -0700

    unused packages gone

--- src/components/forms/Donate/Donateonce.tsx
+++ src/components/forms/Donate/Donateonce.tsx
@@ -96,7 +96,7 @@ export default function Donateonce() {
             defaultChecked
             type="radio"
             name="donation"
-            value="1200"
+            value="120"
             id="1200"
             onChange={handleInputChange}
           />
@@ -107,7 +107,7 @@ export default function Donateonce() {
             step={stepFour}
             type="radio"
             name="donation"
-            value="3000"
+            value="300"
             id="3000"
             onChange={handleInputChange}
           />
