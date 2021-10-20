b22cbc5e1875fedf5ca6e3fd3f2ebab6cb993f59
Jedibezy <Jdocton@gmail.com>
2021-10-19 20:21:33 -0700

    console log for errors on guild

--- src/backend/router/checkout/d/create-checkout.ts
+++ src/backend/router/checkout/d/create-checkout.ts
@@ -5,7 +5,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2020-08-27',
 })
 
-export default async function createCheckoutSession(priceID: number) {
+export default async function createCheckoutSession(priceID: string) {
   const params: Stripe.Checkout.SessionCreateParams = {
     mode: 'subscription',
     payment_method_types: ['card'],
